ae35f898fc744e2bdc2dcedad7b28f9433142f90
guiMendel <guimendeln@gmail.com>
2023-08-25 18:30:50 -0300

    🪲Usar syncedResourceList em usePlayerAPI

--- src/api/hooks/usePlayerAPI.ts
+++ src/api/hooks/usePlayerAPI.ts
@@ -2,22 +2,30 @@ import { Player } from '@/types/'
 import { useResourceAPI } from './useResourceAPI'
 
 export const usePlayerAPI = () => {
-  const { syncResource, desyncResource, syncListResources, getResourceDocRef } =
-    useResourceAPI<Player>('players', (playerUid, playerData) => ({
-      about: playerData.about,
-      email: playerData.email,
-      name: playerData.name,
-      nickname: playerData.nickname,
-      admin: playerData.admin,
-      uid: playerUid,
-      createdAt: new Date(playerData.createdAt),
-      modifiedAt: new Date(playerData.modifiedAt),
-    }))
+  const {
+    syncResource,
+    desyncResource,
+    getResourceDocRef,
+    syncedResourceList,
+    filterResourceList,
+    resourceCollection,
+  } = useResourceAPI<Player>('players', (playerUid, playerData) => ({
+    about: playerData.about,
+    email: playerData.email,
+    name: playerData.name,
+    nickname: playerData.nickname,
+    admin: playerData.admin,
+    uid: playerUid,
+    createdAt: new Date(playerData.createdAt),
+    modifiedAt: new Date(playerData.modifiedAt),
+  }))
 
   return {
     syncPlayer: syncResource,
+    syncedPlayerList: syncedResourceList,
+    filterPlayerList: filterResourceList,
     desyncPlayer: desyncResource,
-    syncListPlayers: syncListResources,
     getPlayerDocRef: getResourceDocRef,
+    playerCollection: resourceCollection,
   }
 }
